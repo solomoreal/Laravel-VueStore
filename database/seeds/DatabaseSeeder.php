0fe60d5aa8c9ca692638974e0af698037b369d58
solomoreal <solomoreal@yahoo.com>
2019-07-22 14:19:16 +0100

    implemented my model,routes, controller, set up passport and vue

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,14 @@
 <?php
 
-use Illuminate\Database\Seeder;
+    use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
+    class DatabaseSeeder extends Seeder
     {
-        // $this->call(UsersTableSeeder::class);
+        public function run()
+        {
+            $this->call([
+              UsersTableSeeder::class,
+              ProductsTableSeeder::class,
+            ]);
+        }
     }
-}
